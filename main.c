e2297be2d9d20e353862fe304c072673d2108973
ugonight <amihajime@yahoo.co.jp>
2016-11-01 19:59:21 +0900

    へいきん
    結局配列の要素数が測れなくて鬱

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
+#include"sum.h"
+#include"average.h"
 
 int main() {
 	int payments[5], i;
 
 	for (i=0; i<5; i++) {
-		scanf("%d", &payments[i]);
+		printf("pay > ");
+		scanf_s("%d", &payments[i], 4);
 	}
+
+	printf("sum : %d\n", sum(payments));
+	printf("average : %d\n", average(payments));
+
 
 	return;
 }
