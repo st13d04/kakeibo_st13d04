b78993db86a7396cfc8fbed4e48dba53abd76490
Hajime ISOBE <st13d04@gm.ibaraki-ct.ac.jp>
2016-11-01 15:47:53 +0900

    sizeofが言うこと聞かない

--- average.c
+++ average.c
@@ -2,7 +2,7 @@
 
 int average(int payments[]) {
 	int result = 0,i;
-	int num = sizeof(payments) / sizeof(payments[0]);
+	int num = sizeof(payments) / sizeof(int);
 
 	for (i=0; i<num; i++) {
 		result += payments[i];
--- sum.c
+++ sum.c
@@ -1,12 +1,15 @@
 #include"sum.h"
+
+#include<stdio.h>
  
 int sum(int payments[]) {
 	int result = 0,i;
-	int num = sizeof(payments) / sizeof(payments[0]);
+	int num = sizeof(payments) / sizeof(int);
 
 	for (i=0; i<num; i++) {
 		result += payments[i];
 	}
+
 
 	return result;
 }
